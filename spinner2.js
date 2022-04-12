2dbbfc6e3ea5c12c2512eed3ba513511806a8aa6
Emma Grannis <grannis.emma@gmail.com>
2022-04-12 19:33:57 +0000

    Functional code, passed lint check. Will revisit later for efficiency/refactoring

--- spinner2.js
+++ spinner2.js
@@ -1,9 +1,10 @@
-// I want to repeat setTimeout 8 times 
 let lineAnimation = ['\r|   ','\r/   ', '\r-   ', '\r\\   ','\r|   ','\r/   ', '\r-   ', '\r\\   ','\r|   '];
 let timer = 100;
 for (let i = 0; i < lineAnimation.length; i++) {
   let char = lineAnimation[i];
   setTimeout(() => process.stdout.write(char), timer);
   timer += 200;
-};
+}
 setTimeout(() => console.log(), timer + 200);
+
+// I want to repeat setTimeout 8 times. Revisit this because I shouldn't need an array of 8 items, want to do this 2x
